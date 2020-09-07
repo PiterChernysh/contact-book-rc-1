5b2d5df38d56041a1efb8f442ff1f6c53f060055
Piter Chernysh <53657929+PiterChernysh@users.noreply.github.com>
2020-09-07 23:13:15 +0300

    Delete dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongodb: {
-    uri: "mongodb+srv://<4p20017@gmail.com>:<1_ZayaKoxana>@contact-book-wwxu0.mongodb.net/test?retryWrites=true&w=majority",
-    options: { useNewUrlParser: true }
-  }
-};
