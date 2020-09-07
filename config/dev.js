037da38f2221fcc46bbf3858c140a892220c44ed
Piter Chernysh <53657929+PiterChernysh@users.noreply.github.com>
2020-09-07 23:09:02 +0300

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongodb: {
-    uri: "mongodb+srv://<4p20017@gmail.com>:<1_ZayaKoxana>@contact-book-wwxu0.mongodb.net/test?retryWrites=true&w=majority",
+    uri: "",
     options: { useNewUrlParser: true }
   }
 };
